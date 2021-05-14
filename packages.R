ab7c7a23e954c3e2ed33c981370df04dabc4d749
Reginal Exavier <reginalexavier@rocketmail.com>
2021-05-14 13:13:49 -0400

    first commit eco cafe

---
+++ packages.R
@@ -0,0 +1,26 @@
+# packages utils
+
+# On CRAN
+install.packages("xaringan")
+install.packages("xaringanthemer")
+install.packages("metathis")
+install.packages("showtext")
+
+# From GitHub
+# install.packages("remotes")
+remotes::install_github("gadenbuie/xaringanExtra")
+remotes::install_github("jhelvy/xaringanBuilder")
+remotes::install_github("gadenbuie/countdown")
+remotes::install_github("gadenbuie/applause")
+remotes::install_github("rstudio/fontawesome")
+remotes::install_github("mitchelloharawild/icons")
+remotes::install_github("mitchelloharawild/icons")
+remotes::install_github("hadley/emo")
+
+
+# links
+# https://www.garrickadenbuie.com/
+# https://slides.garrickadenbuie.com/toronto-data-workshop/#1
+# https://github.com/gadenbuie/slides/blob/gh-pages/toronto-data-workshop/index.Rmd
+# https://spcanelon.github.io/xaringan-basics-and-beyond/index.html
+#
